3f17fb86f4421670d8ca82986125d4e69f47dd0c
Wei Chen <weichenrs@whu.edu.cn>
2023-03-16 16:26:40 +0800

    Update retrain_mfv.sh

--- mfv/retrain_mfv.sh
+++ mfv/retrain_mfv.sh
@@ -1,5 +1,5 @@
 #gid
-#CUDA_VISIBLE_DEVICES=0 python retrain_mfv.py --dataset gid --checkname MMM_DD_DATASET_MODEL_SCALE --batch-size 2 --workers 2 --epochs 20
+CUDA_VISIBLE_DEVICES=0 python retrain_mfv.py --dataset gid --checkname MMM_DD_DATASET_MODEL_SCALE --batch-size 2 --workers 2 --epochs 20
 
 #wfv
 #CUDA_VISIBLE_DEVICES=0 python retrain_mfv.py --dataset wfv --checkname MMM_DD_DATASET_MODEL_SCALE --batch-size 2 --workers 2 --epochs 20
